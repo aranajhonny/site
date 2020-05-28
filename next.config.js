9ea18949672b48114685b5c992495d5a42056463
aranajhonny <gcr-jhonny@modular-silicon-111805.iam.gserviceaccount.com>
2020-05-28 13:12:21 -0500

    a

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
-const isProd = process.env.NODE_ENV === "production";
-
 module.exports = {
-  assetPrefix: isProd ? "https://ayva-demo.imfast.io" : "",
   experimental: {
     reactRefresh: true,
   },
